ec15110b547fb517efcef52e76c278d68ad62850
Zhang <scottzhang235@gmail.com>
2015-12-12 16:54:15 -0800

    adds in styles

--- www/scripts/profile/profile.js
+++ www/scripts/profile/profile.js
@@ -39,12 +39,11 @@
            enabled: false, // whether dragging items is supported
         }
     };
+    $scope.mainTrait = { sizeX: 2, sizeY: 2, row: 0, col: 0 };
+    $scope.categoryPicker = { sizeX: 2, sizeY: 1, row: 2, col: 0 };
     $scope.traits = [
-      { sizeX: 2, sizeY: 2, row: 0, col: 0 },
       { row: 0, col: 2 },
       { row: 1, col: 2 },
-      { row: 2, col: 0 },
-      { row: 2, col: 1 },
       { row: 2, col: 2 },
       { row: 3, col: 0 },
       { row: 3, col: 1 },
