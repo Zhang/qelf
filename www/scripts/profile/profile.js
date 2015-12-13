bd1202d646dd813b0ec46aa6a37b81e4c5e4d5a6
Zhang <scottzhang235@gmail.com>
2015-12-12 16:08:04 -0800

    testing grid view

--- www/scripts/profile/profile.js
+++ www/scripts/profile/profile.js
@@ -41,14 +41,20 @@
     };
     $scope.traits = [
       { sizeX: 2, sizeY: 2, row: 0, col: 0 },
-      { sizeX: 1, sizeY: 1, row: 0, col: 2 },
-      { sizeX: 1, sizeY: 1, row: 1, col: 2 },
-      { sizeX: 1, sizeY: 1, row: 2, col: 0 },
-      { sizeX: 1, sizeY: 1, row: 2, col: 1 },
-      { sizeX: 1, sizeY: 1, row: 2, col: 2 },
-      { sizeX: 1, sizeY: 1, row: 3, col: 0 },
-      { sizeX: 1, sizeY: 1, row: 3, col: 1 },
-      { sizeX: 1, sizeY: 1, row: 3, col: 2 },
+      { row: 0, col: 2 },
+      { row: 1, col: 2 },
+      { row: 2, col: 0 },
+      { row: 2, col: 1 },
+      { row: 2, col: 2 },
+      { row: 3, col: 0 },
+      { row: 3, col: 1 },
+      { row: 3, col: 2 },
+      { row: 4, col: 0 },
+      { row: 4, col: 1 },
+      { row: 4, col: 2 },
+      { row: 5, col: 0 },
+      { row: 5, col: 1 },
+      { row: 5, col: 2 },
     ];
   });
 })();
