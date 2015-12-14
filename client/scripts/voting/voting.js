e8d43cbfdc952d007cb2c0a5679d90221cea0c20
Zhang <Scottzhang235@gmail.com>
2015-12-14 15:30:31 -0800

    adds a cool effect to card deck

--- client/scripts/voting/voting.js
+++ client/scripts/voting/voting.js
@@ -16,24 +16,78 @@
     });
   });
 
-  module.controller('Voting', function($scope) {
+  module.service('CardManager', function() {
+    return function(cards) {
+      this.display = cards.splice(0, 3);
+      this.fullList = cards;
+      this.swipe = function swipe() {
+        if (!_.isEmpty(this.fullList)) {
+          this.display.push(this.fullList.shift());
+        }
+      };
+    };
+  });
+
+  module.controller('Voting', function($scope, CardManager) {
     $scope.trait = {
       title: 'Trustworthiness'
     };
-    $scope.swipe = function(dir) {
-      console.log(dir);
-    };
-    $scope.contestants = [
+    var cards = [{
+      contestants: [
       {
         name: 'This is scott',
-        img: 'http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg',
+        img: 'https://scontent-sjc2-1.xx.fbcdn.net/hphotos-xpt1/v/t1.0-9/12108274_10101564644192315_7334749882900604967_n.jpg?oh=e0fee8c71ce074b2ba8180193ae610bb&oe=56E3A4FB',
         location: 'SF'
       },
       {
         name: 'This is scott with a much longer name nefrufru',
-        img: 'http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg',
+        img: 'https://scontent-sjc2-1.xx.fbcdn.net/hphotos-xpt1/v/t1.0-9/12227222_10207147550300686_4239573437995010557_n.jpg?oh=f69bd8425f56583f72d4f388c3b56690&oe=57226CE3',
         location: 'San Francisco, California, also France'
-      }
-    ];
+      }]
+    },
+    {
+      contestants: [
+      {
+        name: 'This is scott',
+        img: 'https://scontent-sjc2-1.xx.fbcdn.net/hphotos-xpt1/v/t1.0-9/12108274_10101564644192315_7334749882900604967_n.jpg?oh=e0fee8c71ce074b2ba8180193ae610bb&oe=56E3A4FB',
+        location: 'SF'
+      },
+      {
+        name: 'This is scott with a much longer name nefrufru',
+        img: 'https://scontent-sjc2-1.xx.fbcdn.net/hphotos-xpt1/v/t1.0-9/12227222_10207147550300686_4239573437995010557_n.jpg?oh=f69bd8425f56583f72d4f388c3b56690&oe=57226CE3',
+        location: 'San Francisco, California, also France'
+      }]
+    },
+    {
+      contestants: [
+      {
+        name: 'This is scott',
+        img: 'https://scontent-sjc2-1.xx.fbcdn.net/hphotos-xpt1/v/t1.0-9/12108274_10101564644192315_7334749882900604967_n.jpg?oh=e0fee8c71ce074b2ba8180193ae610bb&oe=56E3A4FB',
+        location: 'SF'
+      },
+      {
+        name: 'This is scott with a much longer name nefrufru',
+        img: 'https://scontent-sjc2-1.xx.fbcdn.net/hphotos-xpt1/v/t1.0-9/12227222_10207147550300686_4239573437995010557_n.jpg?oh=f69bd8425f56583f72d4f388c3b56690&oe=57226CE3',
+        location: 'San Francisco, California, also France'
+      }]
+    },
+    {
+      contestants: [
+      {
+        name: 'This is scott',
+        img: 'https://scontent-sjc2-1.xx.fbcdn.net/hphotos-xpt1/v/t1.0-9/12108274_10101564644192315_7334749882900604967_n.jpg?oh=e0fee8c71ce074b2ba8180193ae610bb&oe=56E3A4FB',
+        location: 'SF'
+      },
+      {
+        name: 'This is scott with a much longer name nefrufru',
+        img: 'https://scontent-sjc2-1.xx.fbcdn.net/hphotos-xpt1/v/t1.0-9/12227222_10207147550300686_4239573437995010557_n.jpg?oh=f69bd8425f56583f72d4f388c3b56690&oe=57226CE3',
+        location: 'San Francisco, California, also France'
+      }]
+    }];
+    $scope.cardManager = new CardManager(cards);
+    $scope.swipe = function(dir) {
+      $scope.cardManager.swipe();
+      console.log(dir);
+    };
   });
 })();
