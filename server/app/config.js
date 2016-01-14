77dac58df2834bc2b319ed4b8f23dae6da3784a3
Zhang <Scottzhang235@gmail.com>
2016-01-14 13:22:57 -0800

    adds is prod

--- server/app/config.js
+++ server/app/config.js
@@ -3,10 +3,16 @@
 const uuid = require('uuid');
 
 const isTest = process.env.NODE_ENV === 'test';
-const mongoURI = isTest ? 'localhost/qelf_test_' + uuid.v4() : (process.env.APP_HOST || 'localhost') + '/qelf';
+const isProd = process.env.NODE_ENV === 'prod';
+const mongoURI = (function getMongoURI() {
+  if (isProd) return 'mongodb://admin:internal1@ds045785.mongolab.com:45785/qelf';
+  if (isTest) return 'localhost/qelf_test_' + uuid.v4();
+  return 'localhost/qelf';
+})();
 
 module.exports = {
   isTest: isTest,
+  isProd: isProd,
   mongoURI: mongoURI,
   mongoVersion: '3.0.x'
 };
