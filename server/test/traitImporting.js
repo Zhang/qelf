ed7bd27325959e2a6007a505b25f07a18a8dec87
Zhang <Scottzhang235@gmail.com>
2016-01-18 17:14:26 -0800

    cleaner test handling

--- server/test/traitImporting.js
+++ server/test/traitImporting.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const traits = require('../bin/traits');
+const traits = require('../scripts/traits');
 const expect = require('expect.js');
 const co = require('co');
 const testUtils = require('./testUtils');
@@ -9,18 +9,12 @@ const _ = require('lodash');
 
 describe('Trait Importing', function() {
   beforeEach(testUtils.clearAll);
-  it('should add all default traits', function(done) {
-    co(function *() {
-      yield traits.addDefault();
-      const addedTraits = yield traitTemplateModel.query({
-        id: { $in: _.map(traits.defaultTraits, 'id') }
-      });
-
-      expect(addedTraits).to.have.length(traits.defaultTraits.length);
-      done();
-    }).catch(function(e) {
-      console.error('error adding traits', e);
-      done(e);
+  it('should add all default traits', co.wrap(function* () {
+    yield traits.addDefault();
+    const addedTraits = yield traitTemplateModel.query({
+      id: { $in: _.map(traits.defaultTraits, 'id') }
     });
-  });
+
+    expect(addedTraits).to.have.length(traits.defaultTraits.length);
+  }));
 });
