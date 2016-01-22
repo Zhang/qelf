b27b2c681bf04b44834a3e89c35abcbdcf3cca13
Zhang <Scottzhang235@gmail.com>
2016-01-22 15:20:16 -0800

    not the issue i thought

--- server/app/controllers/votes/query.js
+++ server/app/controllers/votes/query.js
@@ -6,13 +6,12 @@ const voteModel = require('../../models/vote');
 const _ = require('lodash');
 
 const query = function* query() {
-  var start = new Date();
   const facebookId = this.params.facebookId;
+
   yield createVotes(facebookId);
-  console.log(start - new Date());
   const votes = yield voteModel.query({voterId: facebookId, selected: null});
   const denormalizedVotes = yield denormalizeVotes(votes);
-  console.log(start - new Date());
+
   this.body = _.shuffle(denormalizedVotes);
   this.status = 200;
 };
